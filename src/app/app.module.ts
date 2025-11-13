107ffc4f375e39fbcf5ea9a05cc741b8e8c104c4
MaylingAlexia <mayling.alvarez.vargas@alumnos.uta.cl>
2025-11-13 04:06:05 -0300

    feat: Diagramas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    LoginComponent,
+    PerfilComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    //id: number;
+    //nombre: string;
+    email: string;
+    password: string;
+    rol: 'usuario' | 'admin';
+    //foto?: string;
+}
