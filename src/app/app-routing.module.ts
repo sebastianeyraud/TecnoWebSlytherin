b2b3f2d9d4030d71aa989271917b6530e316dd95
Antonella Butrón <antonella.butron.fuentes@alumnos.uta.cl>
2025-11-15 23:50:21 -0300

    Fix: resolver merge conflicts y agregar ver cartelera

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { CarteleraComponent } from './cartelera/cartelera.component';
 
-const routes: Routes = [
-  { path: 'cartelera', component: CarteleraComponent },
-  { path: '', redirectTo: 'cartelera', pathMatch: 'full' }
-];
-=======
 import { LoginComponent } from './pages/login/login.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { AdminComponent } from './pages/admin/admin.component';
@@ -15,9 +8,11 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
 import { ProteccionGuard } from './guards/proteccion.guard';
 import { ProteccionAdminGuard } from './guards/proteccion-admin.guard';
 
+import { CarteleraComponent } from './cartelera/cartelera.component';
+
 const routes: Routes = [
   {
-    path:'',
+    path: '',
     component: PrincipalComponent
   },
   {
@@ -27,7 +22,7 @@ const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
-    canActivate: [ProteccionGuard,ProteccionAdminGuard]
+    canActivate: [ProteccionGuard, ProteccionAdminGuard]
   },
   {
     path: 'perfil',
@@ -35,12 +30,14 @@ const routes: Routes = [
     canActivate: [ProteccionGuard]
   },
   {
+    path: 'cartelera',
+    component: CarteleraComponent
+  },
+  {
     path: '**',
-    redirectTo: '',
+    redirectTo: ''
   }
 ];
-
->>>>>>> 216bc660d52f0ff57d0b88c400c428c239406a7d
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
